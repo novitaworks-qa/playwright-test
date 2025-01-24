cdd1b9598bfd9450acf6d058c0e7c80397173526
Novita <novita@Zakkys-Mac-mini.local>
2025-01-24 13:34:46 +0700

    Initial commit with Playwright setup and tests

---
+++ tests/test-javan.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect, Page, Response } from '@playwright/test';
+
+test('has title', async ({ page }: { page: Page }) => {
+  await page.goto('https://javan.co.id//');
+  await expect(page).toHaveTitle(/Javan/);
+});
+
+test('check broken links', async ({ page }: { page: Page }) => {
+  await page.goto('https://javan.co.id'); 
+  
+  // Ambil semua tautan di halaman
+  const links = await page.$$eval('a', (links) => links.map((link) => link.href));
+  
+  const maxLinksToTest = 10; 
+  for (let i = 0; i < Math.min(maxLinksToTest, links.length); i++) {
+    const link = links[i];
+    
+    // Mengunjungi setiap link dan memeriksa statusnya
+    const response: Response | null = await page.goto(link);
+    
+    if (response?.status() !== 200) {
+      console.log(`Broken link found: ${link} with status ${response?.status()}`);
+    }
+  }
+});
+
